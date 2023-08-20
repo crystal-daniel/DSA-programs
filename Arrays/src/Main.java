0dfd6db2fe57f1cfae6d23c2b2373069030f93b2
Ranjith Crystal <ranjith05chris@gmail.com>
2023-08-20 20:14:20 +0530

    array operations added

---
+++ Arrays/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class Main {
+    static int [] a;
+    static Scanner in = new Scanner(System.in);
+    static int size;
+    void display()
+    {
+        if(size==0)
+            System.out.println("Array si empty");
+        else {
+            for (int i = 0; i < size; i++) {
+                System.out.print(a[i] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    void insert()
+    {
+        if(size==20)
+            System.out.println("Array size is full");
+        else {
+            System.out.println("Enter the element you want to insert");
+            int n = in.nextInt();
+            System.out.println("Enter the position where you want to insert the element");
+            int pos = in.nextInt();
+            pos--;
+            for (int i = size; i > pos; i--) {
+                a[i] = a[i - 1];
+            }
+            a[pos] = n;
+            size++;
+            System.out.println(n+" is added successfully");
+        }
+    }
+    void delete()
+    {
+        if(size==0)
+            System.out.println("Array is empty");
+        else {
+            System.out.println("Enter the position from where you want to delete an element");
+            int pos= in.nextInt();
+            pos--;
+            System.out.println(a[pos]+" is deleted successfully");
+            for (int i = pos; i < size-1; i++) {
+                a[i]=a[i+1];
+            }
+            size--;
+        }
+    }
+    public static void main(String[] args) {
+        Main obj=new Main();
+        System.out.println("Enter the size of the array");
+        size= in.nextInt();
+        a = new int [20];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < size; i++) {
+            a[i]= in.nextInt();
+        }
+        System.out.println("1. Insert an element");
+        System.out.println("2. Delete an element");
+        System.out.println("3. Display the array");
+        System.out.println("4. Exit");
+        int ch;
+        do {
+            System.out.println("Enter your choice: ");
+            ch= in.nextInt();
+            switch (ch) {
+                case 1 -> obj.insert();
+                case 2 -> obj.delete();
+                case 3 -> obj.display();
+                case 4 -> System.exit(0);
+                default -> System.out.println("Invalid Input! Enter again");
+            }
+        }while(true);
+    }
+}
