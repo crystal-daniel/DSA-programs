eba122166c9d1dbd5f0e36ac4aa4142e0f22c426
Ranjith Crystal <ranjith05chris@gmail.com>
2023-09-01 20:12:43 +0530

    bogo wort algorithm added

---
+++ Sorting/Bogo Sort/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    List<Integer> bogoSort(List<Integer> a)
+    {
+        while(!isListSorted(a))
+        {
+            Collections.shuffle(a);
+        }
+        return a;
+    }
+    private boolean isListSorted(List<Integer> a) {
+        for (int i = 0; i < a.size()-1; i++) {
+            if(a.get(i)>a.get(i+1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a= new ArrayList<>();
+        Random r=new Random();
+        for (int i = 0; i < 5; i++) {
+            a.add(r.nextInt(100));
+        }
+        System.out.println("Array before sorting:");
+        System.out.println(a);
+        Main obj = new Main();
+        a=obj.bogoSort(a);
+        System.out.println("Array before sorting:");
+        System.out.println(a);
+    }
+}
