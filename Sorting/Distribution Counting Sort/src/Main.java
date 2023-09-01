5bf337f557aae1469377891979fea107bc023331
Ranjith Crystal <ranjith05chris@gmail.com>
2023-09-01 19:43:15 +0530

    distribution counting sort algorithm added

--- Sorting/Distribution Counting Sort/src/Main.java
+++ Sorting/Distribution Counting Sort/src/Main.java
@@ -1,6 +1,30 @@
-public class Main {
-    int [] distributionCountingSort(int [] a, int n)
-    {
+import java.util.Arrays;
 
+public class Main {
+    int [] distributionCountingSort(int [] a, int n, int u, int l)
+    {
+        int [] d = new int [u-l+1],s = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[a[i]-l]=d[a[i]-l]+1;
+        }
+        for (int j = 1; j <= u-l; j++) {
+            d[j]=d[j-1]+d[j];
+        }
+        for (int i = n-1; i >=0 ; i--) {
+            int j=a[i]-l;
+            s[d[j]-1]=a[i];
+            d[j]=d[j]-1;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int [] a = {12,12,13,11,13,12};
+        System.out.println("Array before sorting: ");
+        System.out.println(Arrays.toString(a));
+        Main obj = new Main();
+        a=obj.distributionCountingSort(a,6,13,11);
+        System.out.println("Array before sorting: ");
+        System.out.println(Arrays.toString(a));
     }
 }
