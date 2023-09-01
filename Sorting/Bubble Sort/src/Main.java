fd3edf47712cb616eb4dfb475fd09b9f35aac86f
Ranjith Crystal <ranjith05chris@gmail.com>
2023-09-01 19:14:58 +0530

    bubble sort algorithm added

---
+++ Sorting/Bubble Sort/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Main {
+    void bubbleSort(int [] a,int n)
+    {
+        int i,j,temp;
+        for(i=0;i<n-1;i++)
+        {
+            for(j=0;j<n-1-i;j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int i, n;
+        int [] a;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        n=in.nextInt();
+        a=new int[n];
+        Random random=new Random();
+        for (i = 0; i < n; i++) {
+            a[i]=random.nextInt(100);
+        }
+        System.out.println("Array before sorting: ");
+        for (i = 0; i < n; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        System.out.println();
+        double startTime = System.currentTimeMillis();
+        Main s = new Main();
+        s.bubbleSort(a,n);
+        double stopTime = System.currentTimeMillis();
+        double elapsedTime = stopTime - startTime;
+        System.out.println("Sorted array: ");
+        for (i = 0; i < n; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        System.out.println();
+        System.out.println("Time taken to sort is "+elapsedTime+" milliseconds");
+    }
+}
